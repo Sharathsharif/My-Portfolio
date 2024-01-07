cab57023e7f229b16112879186f644d7151e3652
Sharath <sharathsharif@gmail.com>
2024-01-07 12:45:06 +0530

    finished the footer

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+
+
+function Navbar(){
+    
+
+    return(
+        <nav className="Navbar_container">
+        <h1 >Sharath-Software Developer</h1>
+        </nav>
+    )
+}
+
+export default Navbar;
---
+++ src/footer.js
@@ -0,0 +1,38 @@
+import React from "react";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+function Footer(){
+
+return(
+    <footer className="footermain">
+        <h1>
+            Sharath SS - Software Engineer
+        </h1>
+        <br />
+        
+        <a href="#about" >About |</a>
+        <a href="#projects"> Projects </a>
+        
+        <ul className="footer_socialmedia">
+        <a href="https://www.facebook.com/" target="blank"className="footer__social-link">
+        <i class="bi bi-facebook"></i>
+        </a>
+        <a href="https://www.instagram.com/" target="blank"className="footer__social-link">
+        <i class="bi bi-instagram"></i>
+        </a>
+        <a href="https://github.com/Sharathsharif" target="blank"className="footer__social-link">
+        <i class="bi bi-github"></i>
+        </a>
+
+
+        </ul>
+
+
+
+    </footer>
+
+)
+
+}
+
+export default Footer;
